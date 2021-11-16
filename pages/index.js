0638bc0b1466b75bb2bae312f2aaaf4fc49d7dbb
Nikolay Kharitonov <harnik1988@gmail.com>
2021-11-16 21:18:49 +0300

    json-server

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Link from 'next/link';
-import Head from 'next/head'
 import { MainLayout } from '../components/MainLayout';
 
 export default function Index() {
     return (        
         <MainLayout title ={'Home Page'}>
             <h1>Hello Next.JS!</h1> 
-            <p>Lorem inputa sdhjshdfj sjdfhjksdhjkfh jkgsdkjfjk</p>  
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>  
             <p><Link href="/about"><a>About Page</a></Link></p>
             <p><Link href={"/posts"}><a>Posts Page</a></Link></p>
         </MainLayout>     
--- pages/posts.js
+++ pages/posts.js
@@ -1,10 +1,20 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import { MainLayout } from '../components/MainLayout';
 
 export default function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(async () => {
+        const responce = await fetch('http://localhost:4200/posts')
+        const data = await responce.json()
+        setPosts(data);
+    }, [])
+
     return (
         <MainLayout title ={'Post Page'}>             
             <h1>Posts Page</h1>
+            <pre>{JSON.stringify(posts)}</pre>
         </MainLayout>
        
     )  
