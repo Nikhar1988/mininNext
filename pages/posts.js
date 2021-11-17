7f8353134deb8b121d9af7ac7b65663e5007e0ae
Nikolay Kharitonov <harnik1988@gmail.com>
2021-11-17 20:33:25 +0300

    gitInitialProps

--- pages/posts.js
+++ pages/posts.js
@@ -1,21 +1,38 @@
-import Head from 'next/head';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { MainLayout } from '../components/MainLayout';
 
-export default function Posts() {
-    const [posts, setPosts] = useState([]);
+export default function Posts({posts}) {
+    // const [posts, setPosts] = useState([]);
 
-    useEffect(async () => {
-        const responce = await fetch('http://localhost:4200/posts')
-        const data = await responce.json()
-        setPosts(data);
-    }, [])
+    // useEffect(async () => {
+    //     const responce = await fetch('http://localhost:4200/posts')
+    //     const data = await responce.json()
+    //     setPosts(data);
+    // }, [])
 
     return (
         <MainLayout title ={'Post Page'}>             
             <h1>Posts Page</h1>
-            <pre>{JSON.stringify(posts)}</pre>
+            <ul>
+               {posts.map(post => (
+                   <li key={post.id}>
+                       <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+                           <a>
+                               {post.title}
+                            </a>
+                        </Link>
+                   </li>
+               ))} 
+            </ul>
         </MainLayout>
        
     )  
+}
+Posts.getInitialProps = async () => {
+    const responce = await fetch('http://localhost:4200/posts')
+    const posts = await responce.json();
+    return{
+        posts
+    }
 }
