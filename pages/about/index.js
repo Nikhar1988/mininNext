db0871f41b3d5a54314d8f46b73821a115d91ad3
Nikolay <harnik1988@gmail.com>
2021-11-19 15:47:53 +0300

    change

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,14 +3,14 @@ import { MainLayout } from "../../components/MainLayout";
 
 export default function About({title}) {
     
-    const linkClickHendler =()=>{
+    const linkClickHandler =()=>{
         Router.push('/')
     }
     
     return (
         <MainLayout title ={'About Page'}>
-            <h1>{JSON.stringify(title)}</h1>
-            <button onClick={linkClickHendler}>Go back to home</button>
+            <h1>{title}</h1>
+            <button onClick={linkClickHandler}>Go back to home</button>
             <button onClick={()=> Router.push('/posts')}>Go to posts</button>
         </MainLayout>
        
